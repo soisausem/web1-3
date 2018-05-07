8f8a6f70989b66aa51800e133206cfb25156627d
Unknown <g1827403@st.t-kougei.ac.jp>
2018-05-07 17:55:39 +0900

    18.05.07.commit

---
+++ Sample/Assets/VectorTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector2 playerPos = new Vector2(8.0f, 2.0f);
+        Vector2 startPos = new Vector2(1.8f, 3.5f);
+        Vector2 endPos = new Vector2(8.8f, 5.5f);
+        Vector2 dir = endPos - startPos;
+        playerPos.x += 2.2f;
+        playerPos.y -= 0.1f;
+        Debug.Log(playerPos);
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Sample/Assets/web4Test.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob
+{
+    public string name = "モブ敵";
+    public int hp = 200;
+}
+
+public class Player
+{
+    Mob youMob = new Mob();
+    private string name = "勇者君";
+    private int hp = 100;
+    private int power = 50;
+
+    public int Attack(int x)
+    {
+        youMob.hp -= this.power;
+        Debug.Log(this.name + "の攻撃！");
+        Debug.Log(this.power + "のダメージを与えた！");
+        if (youMob.hp < 0)
+        {
+            Debug.Log(youMob.name + "を倒した！");
+            return 3;
+        }
+        return x;
+    }
+    public int Damege(int damege,int x)
+    {
+        this.hp -= damege;
+
+        Debug.Log(youMob.name + "の攻撃！");
+            Debug.Log(damege + "のダメージを受けた");
+        if (this.hp < 0) this.hp = 0;
+            Debug.Log("残りHP" + this.hp);
+        if (this.hp <= 0)
+        {
+            Debug.Log(this.name + "はしんでしまった・・・");
+            return 4;
+        }
+        return x;
+    }
+    public void Power(int ATK)
+    {
+        if (ATK > this.power)
+        {
+            this.power = ATK;
+            Debug.Log("武器を拾った.攻撃力が" + this.power + "になった");
+        }
+        else
+        {
+            Debug.Log("武器を拾ったけどなんか弱そうなので捨てた");
+        }
+    }
+}
+public class web4Test : MonoBehaviour {
+    int Lulet(int x)
+    {
+        int y = x % 3;
+        return y;
+    }
+
+    
+	// Use this for initialization
+	void Start () {
+
+        Player myPlayer = new Player();
+        int y;
+        int[] lulet = { 21, 34, 72, 23, 66, 56, 12, 55, 200, 15 };
+
+        for (int i = 0; i < lulet.Length; i++)
+        {
+            y = Lulet(lulet[i]);
+            Debug.Log(i + 1 + "ターン目です");
+            if (y == 0) y = myPlayer.Attack(y);
+            else if (y == 1) y = myPlayer.Damege(lulet[i], y);
+            else if (y == 2) myPlayer.Power(lulet[i]);
+            if (y == 3)
+            {
+                Debug.Log("戦いは終わった！");
+                break;
+            }
+            else if (y == 4)
+            {
+                Debug.Log("しんでしまうとはなさけない！");
+                break;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
